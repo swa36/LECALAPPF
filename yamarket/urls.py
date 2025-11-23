2f7ffca3d538717598ae9c9761ef095a559e2f37
swa-home-PC <swa36@yandex.ru>
2025-11-23 20:10:36 +0300

    Обновление интеграции Я.Маркет

--- yamarket/urls.py
+++ yamarket/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from yamarket.views import updateStock, newOrder, statusOrder
+from yamarket.views import updateStock, newOrder, statusOrder,getNotifyYaMarket
 
 app_name = 'yaMarket'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('stocks', updateStock, name='updateStockYaMarket'),
     path('order/accept', newOrder, name='newOrder'),
     path('order/status', statusOrder, name='newOrder'),
+    path('notyify/', getNotifyYaMarket, name='notifyYaMarket'),
 
 ]
